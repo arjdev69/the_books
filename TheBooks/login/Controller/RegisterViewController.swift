56e9f68478dab557280ea70f04e9125207bf95e7
ArjDev69 <bruno96araujo@gmail.com>
2021-04-01 01:26:50 -0300

    create init layout register view

---
+++ TheBooks/login/Controller/RegisterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterViewController.swift
+//  TheBooks
+//
+//  Created by ArjMaster on 31/03/21.
+//  Copyright © 2021 ArjMaster. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func btnBack(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
