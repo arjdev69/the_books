b7e8856a83eab93c823c31ecf54840788ac149b0
ArjDev69 <bruno96araujo@gmail.com>
2021-04-11 18:20:27 -0300

    create layout edit view book

---
+++ TheBooks/book/edit/Controller/EditBookViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EditBookViewController.swift
+//  TheBooks
+//
+//  Created by ArjMaster on 11/04/21.
+//  Copyright © 2021 ArjMaster. All rights reserved.
+//
+
+import UIKit
+
+class EditBookViewController: UIViewController {
+
+    @IBOutlet weak var Header: UIView!
+    @IBOutlet weak var titleBook: UILabel!
+    
+    @IBOutlet weak var FieldsFormView: UIView!
+    @IBOutlet weak var titleFieldForm: UITextField!
+    @IBOutlet weak var authorFieldForm: UITextField!
+    @IBOutlet weak var genreFieldForm: UIButton!
+    @IBOutlet weak var statusFieldForm: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupLayout()
+        self.setTextFieldForm()
+    }
+    
+    //MARK: SETUP LAYOUT
+    func setupLayout(){
+        Utils().setColorBorderView(view: self.FieldsFormView, border: 1, radius: 14)
+    }
+    
+    //MARK: FUNCS SUPPORT
+    func setTextFieldForm(){
+        self.titleBook.text = "Crépusculo".uppercased()
+        self.titleFieldForm.text = "Crépusculo"
+        self.authorFieldForm.text = "Silva, João."
+        self.genreFieldForm.setTitle("Suspense, Romance.", for: .normal)
+        self.statusFieldForm.setTitle("Lendo", for: .normal)
+    }
+}
